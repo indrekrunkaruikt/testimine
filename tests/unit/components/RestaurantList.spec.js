439d2341dac2da8dc61adbcc5b81f62013686fd5
indrek <indrek.runkaur@khk.ee>
2021-05-02 14:59:50 +0300

    Shortenede the tests

--- tests/unit/components/RestaurantList.spec.js
+++ tests/unit/components/RestaurantList.spec.js
@@ -6,29 +6,19 @@ describe('RestaurantList', () => {
   const localVue = createLocalVue();
   localVue.use(Vuex);
 
-  it('loads restaurants on mount', () => {
-    const restaurantsModule = {
-      namespaced: true,
-      actions: {
-        load: jest.fn().mockName('load'),
-      },
-    };
-    const store = new Vuex.Store({
-      modules: {
-        restaurants: restaurantsModule,
-      },
-    });
-    mount(RestaurantList, {localVue, store});
+  const records = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
 
-    expect(restaurantsModule.actions.load).toHaveBeenCalled();
-  });
-  it('displays the restaurants', () => {
-    const records = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
+  let restaurantsModule;
+  let wrapper;
 
-    const restaurantsModule = {
+  const findByTestId = (wrapper, testId, index) =>
+  wrapper.findAll(`[data-testid="${testId}"]`).at(index);
+
+  beforeEach(() => {
+    restaurantsModule = {
       namespaced: true,
       state: {records},
       actions: {
@@ -40,17 +30,14 @@ describe('RestaurantList', () => {
         restaurants: restaurantsModule,
       },
     });
-    const wrapper = mount(RestaurantList, {localVue, store});
-    const firstRestaurantName = wrapper
-      .findAll('[data-testid="restaurant"]')
-      .at(0)
-      .text();
-    expect(firstRestaurantName).toBe('Sushi Place');
 
-    const secondRestaurantName = wrapper
-      .findAll('[data-testid="restaurant"]')
-      .at(1)
-      .text();
-    expect(secondRestaurantName).toBe('Pizza Place');
+    wrapper = mount(RestaurantList, {localVue, store});
+  });
+  it('loads restaurants on mount', () => {
+    expect(restaurantsModule.actions.load).toHaveBeenCalled();
+  });
+  it('displays the restaurants', () => {
+    expect(findByTestId(wrapper, 'restaurant', 0).text()).toBe('Sushi Place');
+    expect(findByTestId(wrapper, 'restaurant', 1).text()).toBe('Pizza Place');
   });
 });
