c5a7985b8ca53777d603ce61092afdf33a0535d0
indrek <indrek.runkaur@khk.ee>
2021-05-11 15:48:38 +0300

    stepping back up

--- src/components/NewRestaurantForm.vue
+++ src/components/NewRestaurantForm.vue
@@ -1,12 +1,54 @@
 <template>
-  <form>
-    <v-text-field placeholder="Add Restaurant" filled type="text" />
-    <v-btn color="primary" class="black--text"> Add </v-btn>
+  <form @submit.prevent="handleSave">
+    <v-alert
+      v-if="validationError"
+      type="error"
+      data-testid="new-restaurant-name-error"
+    >
+      Name is required.
+    </v-alert>
+    <v-text-field
+      placeholder="Add Restaurant"
+      filled
+      type="text"
+      v-model="name"
+      data-testid="new-restaurant-name-field"
+    />
+    <v-btn
+      type="submit"
+      color="primary"
+      class="black--text"
+      data-testid="new-restaurant-submit-button"
+    >
+      Add
+    </v-btn>
   </form>
 </template>
 
 <script>
+import {mapActions} from 'vuex';
 export default {
   name: 'NewRestaurantForm',
+  data() {
+    return {
+      name: '',
+      validationError: false,
+    };
+  },
+  methods: {
+    ...mapActions({
+      createRestaurant: 'restaurants/create',
+    }),
+    handleSave() {
+      if (this.name) {
+        this.validationError = false;
+      } else {
+        this.validationError = true;
+      }
+      this.createRestaurant(this.name).then(() => {
+        this.name = '';
+      });
+    },
+  },
 };
 </script>
